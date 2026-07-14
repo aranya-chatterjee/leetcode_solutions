02035cd066ffe79b980df18cf742530c1777c878
ARANYA <143878322+aranya-chatterjee@users.noreply.github.com>
2026-07-14 17:07:51 +0530

    Time: 3 ms (91.35%), Space: 19.8 MB (76.34%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        path=[]
+        used=[False]*len(nums)
+        def backtracking():
+            if len(nums)==len(path):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i>0 and nums[i]==nums[i-1] and not used[i-1]:
+                    continue
+                used[i]=True
+                path.append(nums[i])
+                backtracking()
+                path.pop()
+
+                used[i]=False
+        backtracking()
+        return res
+        
